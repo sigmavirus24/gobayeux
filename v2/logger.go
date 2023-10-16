7c341b04b222405db22820e3648d3693c634a986
Kenny Parnell <k.parnell@gmail.com>
2023-10-16 15:10:56 -0400

    Add functions for additional error levels and wrap slog.

--- v2/logger.go
+++ v2/logger.go
@@ -4,17 +4,21 @@ import "github.com/sirupsen/logrus"
 
 // Logger defines the logging interface gobayeux leverages
 type Logger interface {
-	// Debug takes any number of arguments and logs them at the debug level
-	Debug(args ...any)
+	// Debug takes a message and any number of arguments and logs them at the
+	// debug level
+	Debug(msg string, args ...any)
 
-	// Info takes any number of arguments and logs them at the info level
-	Info(args ...any)
+	// Info takes a message and any number of arguments and logs them at the
+	// info level
+	Info(msg string, args ...any)
 
-	// Warn takes any number of arguments and logs them at the info level
-	Warn(args ...any)
+	// Warn takes a message and any number of arguments and logs them at the
+	// warn level
+	Warn(msg string, args ...any)
 
-	// Error takes any number of arguments and logs them at the info level
-	Error(args ...any)
+	// Error takes a message and any number of arguments and logs them at the
+	// error level
+	Error(msg string, args ...any)
 
 	// WithError returns a new Logger that addes the given error to any log
 	// messages emitted
@@ -28,16 +32,16 @@ type Logger interface {
 type nullLogger struct {
 }
 
-func (*nullLogger) Debug(args ...any) {
+func (*nullLogger) Debug(msg string, args ...any) {
 }
 
-func (*nullLogger) Info(args ...any) {
+func (*nullLogger) Info(msg string, args ...any) {
 }
 
-func (*nullLogger) Warn(args ...any) {
+func (*nullLogger) Warn(msg string, args ...any) {
 }
 
-func (*nullLogger) Error(args ...any) {
+func (*nullLogger) Error(msg string, args ...any) {
 }
 
 func (l *nullLogger) WithError(err error) Logger {
@@ -54,6 +58,22 @@ func newNullLogger() *nullLogger {
 
 type wrappedFieldLogger struct {
 	logrus.FieldLogger
+}
+
+func (w *wrappedFieldLogger) Debug(msg string, args ...any) {
+	w.FieldLogger.Debug(append([]any{msg}, args...))
+}
+
+func (w *wrappedFieldLogger) Info(msg string, args ...any) {
+	w.FieldLogger.Info(append([]any{msg}, args...))
+}
+
+func (w *wrappedFieldLogger) Warn(msg string, args ...any) {
+	w.FieldLogger.Warn(append([]any{msg}, args...))
+}
+
+func (w *wrappedFieldLogger) Error(msg string, args ...any) {
+	w.FieldLogger.Error(append([]any{msg}, args...))
 }
 
 func (w *wrappedFieldLogger) WithError(err error) Logger {
